d10afac42bd2c8cb071aaf8d5b8596aef51135f3
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-01-31 18:46:33 +0100

    feat: creation du model Inscription

---
+++ backend-admin-mns/src/main/java/com/mns/admin/model/Inscription.java
@@ -0,0 +1,32 @@
+package com.mns.admin.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "inscription", uniqueConstraints = @UniqueConstraint(columnNames = {"id_stagiaire", "id_formation"}))
+public class Inscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_inscription")
+    private Long idInscription;
+
+    @ManyToOne
+    @JoinColumn(name = "id_stagiaire", nullable = false)
+    private Utilisateur stagiaire;
+
+    @ManyToOne
+    @JoinColumn(name = "id_formation", nullable = false)
+    private Formation formation;
+
+    @Column(name = "date_inscription", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateInscription;
+}
