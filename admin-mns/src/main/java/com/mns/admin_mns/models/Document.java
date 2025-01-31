85d2d55cc74165a2a610fed5f3754e076e4282b8
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-01-31 12:32:48 +0100

    (feat): Ajout du models Document

---
+++ admin-mns/src/main/java/com/mns/admin_mns/models/Document.java
@@ -0,0 +1,51 @@
+package com.mns.admin_mns.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "document")
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_document")
+    private Long idDocument;
+
+    @ManyToOne
+    @JoinColumn(name = "id_dossier", nullable = false)
+    private Dossier dossier;
+
+    @ManyToOne
+    @JoinColumn(name = "id_typeDocument", nullable = false)
+    private TypeDocument typeDocument;
+
+    @ManyToOne
+    @JoinColumn(name = "id_statut", nullable = false)
+    private Statut statut;
+
+    @ManyToOne
+    @JoinColumn(name = "id_secret")
+    private Secret secret;
+
+    @Column(name = "dateDepot_document", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateDepotDocument;
+
+    @Column(name = "dateLimite_document")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateLimiteDocument;
+
+    @Column(name = "cleChiffrement_document", nullable = false)
+    private String cleChiffrement_document;
+
+    @Column(name = "contenuChiffre_document", nullable = false)
+    @Lob
+    private byte[] contenuChiffreDocument;
+}
