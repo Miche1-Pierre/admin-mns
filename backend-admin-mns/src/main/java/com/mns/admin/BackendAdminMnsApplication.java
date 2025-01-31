6f0cca1937393d5cb74936a42765eed78946dae4
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-01-31 18:17:25 +0100

    chore: initialisation de Java Spring Boot

---
+++ backend-admin-mns/src/main/java/com/mns/admin/BackendAdminMnsApplication.java
@@ -0,0 +1,13 @@
+package com.mns.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdminMnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAdminMnsApplication.class, args);
+	}
+
+}
---
+++ backend-admin-mns/src/test/java/com/mns/admin/BackendAdminMnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mns.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdminMnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
