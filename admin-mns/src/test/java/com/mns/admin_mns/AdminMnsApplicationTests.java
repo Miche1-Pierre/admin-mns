761fff8b8c7dc271bbbca9ceffb5f8598cd08dfc
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-01-31 09:00:34 +0100

    (feat): Spring Boot setup.

---
+++ admin-mns/src/test/java/com/mns/admin_mns/AdminMnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mns.admin_mns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminMnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
