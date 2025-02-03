741569865b66a752c03686f526197aecc5b4fea5
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-03 22:27:17 +0100

    feat: Ajout de la page du dashboard principale

---
+++ frontend-admin-mns/public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="/frontend-admin-mns/src/styles/style.css">
+    <!-- Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>ADMIN MNS | Home</title>
+</head>
+
+<body>
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/frontend-admin-mns/src/components/navbar.php"; ?>
+    </header>
+    <main>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/frontend-admin-mns/src/components/breadcrumb.php"; ?>
+    </main>
+    <footer>
+
+    </footer>
+
+    <script src="/frontend-admin-mns/src/scripts/script.js"></script>
+</body>
+
+</html>
