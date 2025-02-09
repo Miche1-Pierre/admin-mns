7a56eebe250fbece5f3c6f5d5fc36b137a5bdf5b
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-09 17:10:01 +0100

    feat: Ajout du composant Navbar

---
+++ frontend-admin-mns/components/navbar.php
@@ -0,0 +1,99 @@
+<div class="navbar">
+    <aside class="sidebar">
+        <div class="menu-btn">
+            <i class='bx bx-chevron-left icon'></i>
+        </div>
+        <div class="head">
+            <div class="logo-details">
+                <p class="details">Admin</p>
+                <img src="/frontend-admin-mns/assets/logo.png" alt="">
+            </div>
+        </div>
+        <div class="nav">
+            <div class="menu">
+                <p class="title">Main</p>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class='bx bx-home-alt'></i>
+                            <span class="text">Home</span>
+                        </a>
+                    </li>
+                    <li class="active">
+                        <a href="#">
+                            <i class='bx bx-grid-alt'></i>
+                            <span class="text">Modules</span>
+                            <i class='bx bx-chevron-down'></i>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="#">
+                                    <span class="text">Applications</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="text">Absences & Lateness</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class='bx bx-user'></i>
+                            <span class="text">Users</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class='bx bx-folder'></i>
+                            <span class="text">Documents</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class='bx bx-dots-horizontal-rounded'></i>
+                            <span class="text">More</span>
+                            <i class='bx bx-chevron-down'></i>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="#">
+                                    <span class="text">Nothing</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="text">Nothing</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="menu">
+            <p class="title">Account</p>
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-log-out'></i>
+                        <span class="text">Logout</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-cog'></i>
+                        <span class="text">Settings</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-question-mark'></i>
+                        <span class="text">Help</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </aside>
+</div>
