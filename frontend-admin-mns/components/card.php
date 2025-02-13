f0e975b6b77ab3216caae20bf74004f8f5539cae
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-13 12:23:33 +0100

    feat: Ajout du model de carte / widget

--- frontend-admin-mns/components/card.php
+++ frontend-admin-mns/components/card.php
@@ -1,15 +1,37 @@
 <?php
-function generateCard($title, $text, $img, $link)
+function generateCard($title, $link, $text = null, $img = null, $chartId = null, $chartType = null, $labels = [], $data = [], $bgColor = [], $borderColor = [], $options = [])
 {
 ?>
     <div class="card">
         <div class="card-container" id="card-container">
             <h2><?= htmlspecialchars($title) ?></h2>
-            <p><?= htmlspecialchars($text) ?></p>
-            <img src="<?= htmlspecialchars($img) ?>" alt="Thumbnail">
+
+            <?php if ($text): ?>
+                <p><?= htmlspecialchars($text) ?></p>
+            <?php endif; ?>
+
+            <?php if ($img): ?>
+                <img src="<?= htmlspecialchars($img) ?>" alt="Thumbnail">
+            <?php endif; ?>
+
+            <?php if ($chartId): ?>
+                <canvas id="<?= htmlspecialchars($chartId) ?>"></canvas>
+                <script>
+                    window.chartData = window.chartData || [];
+                    window.chartData.push({
+                        chartId: <?= json_encode($chartId) ?>,
+                        chartType: <?= json_encode($chartType) ?>,
+                        labels: <?= json_encode($labels) ?>,
+                        data: <?= json_encode($data) ?>,
+                        bgColor: <?= json_encode($bgColor) ?>,
+                        borderColor: <?= json_encode($borderColor) ?>,
+                        options: <?= json_encode($options) ?>
+                    });
+                </script>
+            <?php endif; ?>
+
             <div class="buttons">
-                <a href="<?= htmlspecialchars($link) ?>" target="_blank" class="button secondary">Test</a>
-                <a href="" class="button primary">Test</a>
+                <a href="<?= htmlspecialchars($link) ?>" class="button primary">Voir Plus</a>
             </div>
         </div>
     </div>
