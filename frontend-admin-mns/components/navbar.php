36e12130afe95e05b6890c930d719ecf81b07c72
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-15 11:46:41 +0100

    feat: ajout de pointeur pour la naviguation AJAX

--- frontend-admin-mns/components/navbar.php
+++ frontend-admin-mns/components/navbar.php
@@ -14,26 +14,26 @@
                 <p class="title">Main</p>
                 <ul>
                     <li class="active">
-                        <a href="#">
+                        <a href="#" class="ajax-link" data-page="home">
                             <i class='bx bx-home-alt'></i>
                             <span class="text">Home</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a>
                             <i class='bx bx-grid-alt'></i>
                             <span class="text">Modules</span>
                             <i class='bx bx-chevron-down'></i>
                         </a>
                         <ul class="sub-menu">
                             <li>
-                                <a href="#">
+                                <a href="#" class="ajax-link" data-page="modules" data-subpage="candidatures">
                                     <i class='bx bxs-id-card'></i>
                                     <span class="text">Applications</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="#">
+                                <a href="#" class="ajax-link" data-page="modules" data-subpage="absences">
                                     <i class='bx bx-timer'></i>
                                     <span class="text">Absences & Lateness</span>
                                 </a>
@@ -41,38 +41,38 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" class="ajax-link" data-page="users">
                             <i class='bx bx-user'></i>
                             <span class="text">Users</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" class="ajax-link" data-page="documents">
                             <i class='bx bx-folder'></i>
                             <span class="text">Documents (GED)</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" class="ajax-link" data-page="messaging">
                             <i class='bx bx-mail-send'></i>
                             <span class="text">Messaging</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a>
                             <i class='bx bx-dots-horizontal-rounded'></i>
                             <span class="text">More</span>
                             <i class='bx bx-chevron-down'></i>
                         </a>
                         <ul class="sub-menu">
                             <li>
-                                <a href="#">
+                                <a href="#" class="ajax-link" data-page="more" data-subpage="stats">
                                     <i class='bx bx-line-chart'></i>
                                     <span class="text">Stats</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="#">
+                                <a href="#" class="ajax-link" data-page="more" data-subpage="settings">
                                     <i class='bx bx-cog'></i>
                                     <span class="text">Settings (Admin)</span>
                                 </a>
@@ -92,13 +92,13 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="#" class="ajax-link">
                         <i class='bx bx-cog'></i>
                         <span class="text">Settings</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="#" class="ajax-link">
                         <i class='bx bx-question-mark'></i>
                         <span class="text">Help</span>
                     </a>
