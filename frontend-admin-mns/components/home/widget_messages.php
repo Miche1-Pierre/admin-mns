46e60b4a90ae1e7476ebd8479d58043b1583f79a
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-15 11:48:38 +0100

    feat: ajout du widget mssage sous forme de tableau

--- frontend-admin-mns/components/home/widget_messages.php
+++ frontend-admin-mns/components/home/widget_messages.php
@@ -4,8 +4,26 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/frontend-admin-mns/components/card.ph
 function widgetMessages() {
     $title = "Messagerie";
     $text = "Consultez et envoyez vos messages.";
-    $img = "assets/icons/messages.png";
     $link = "#";
+    $img = null;
 
-    generateCard($title, $text, $img, $link);
+    $messages = [
+        ["expediteur" => "Admin", "contenu" => "Rappel: réunion demain à 10h.", "heure" => "10:30"],
+        ["expediteur" => "Professeur X", "contenu" => "Votre devoir est à rendre vendredi.", "heure" => "09:15"],
+        ["expediteur" => "RH", "contenu" => "Votre attestation est disponible.", "heure" => "08:50"],
+        ["expediteur" => "Candidature", "contenu" => "Votre dossier est en cours de traitement.", "heure" => "08:30"],
+        ["expediteur" => "Secrétariat", "contenu" => "Votre document a été validé.", "heure" => "08:00"]
+    ];
+
+    $content = "<table class='table-widget'><thead><tr><th>Expéditeur</th><th>Message</th><th>Heure</th></tr></thead><tbody>";
+    foreach ($messages as $msg) {
+        $content .= "<tr>
+                        <td>" . htmlspecialchars($msg["expediteur"]) . "</td>
+                        <td>" . htmlspecialchars($msg["contenu"]) . "</td>
+                        <td>" . htmlspecialchars($msg["heure"]) . "</td>
+                     </tr>";
+    }
+    $content .= "</tbody></table>";
+
+    generateCard($title, $link, $content, $img);
 }
