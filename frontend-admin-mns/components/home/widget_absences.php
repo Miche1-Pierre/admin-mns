a3c6f6100dc99fcb34907021c3a8831cdd0008f4
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-15 11:47:33 +0100

    feat: passage de data en dataset

--- frontend-admin-mns/components/home/widget_absences.php
+++ frontend-admin-mns/components/home/widget_absences.php
@@ -1,11 +1,40 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . "/frontend-admin-mns/components/card.php";
 
-function widgetAbsences() {
+function widgetAbsences()
+{
     $title = "Absences & Retards";
-    $text = "Consultez vos absences et retards.";
-    $img = "assets/icons/absence.png";
     $link = "#";
+    $text = "Consultez les absences et retards";
+    $img = null;
+    $chartId = "absencesChart";
+    $chartType = "bar";
 
-    generateCard($title, $text, $img, $link);
+    $labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    $absencesData = [2, 5, 3, 12, 6, 2, 4, 3, 9, 8, 3, 6];
+    $retardsData = [1, 3, 2, 6, 4, 1, 3, 2, 5, 4, 2, 3];
+
+    $datasets = [
+        [
+            "label" => "Absences",
+            "data" => $absencesData,
+            "backgroundColor" => "#2B2D42",
+            "borderColor" => "#eeeeee",
+            "borderWidth" => 1
+        ],
+        [
+            "label" => "Retards",
+            "data" => $retardsData,
+            "backgroundColor" => "#D90429",
+            "borderColor" => "#eeeeee",
+            "borderWidth" => 1
+        ]
+    ];
+
+    $options = [
+        "responsive" => true,
+        "maintainAspectRatio" => false
+    ];
+
+    generateCard($title, $link, $text, $img, $chartId, $chartType, $labels, $datasets, $options);
 }
