f7b2bd5032cb1cfa84c58788e75778adc137222d
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-15 18:30:52 +0100

    feat: appelle Ajax de home.php

--- frontend-admin-mns/components/home/home.php
+++ frontend-admin-mns/components/home/home.php
@@ -8,3 +8,9 @@ widgetAbsences();
 widgetCandidatures();
 widgetDocuments();
 widgetMessages();
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+if ($page === "home") {
+    echo '<script src="/frontend-admin-mns/js/home.js"></script>';
+}
