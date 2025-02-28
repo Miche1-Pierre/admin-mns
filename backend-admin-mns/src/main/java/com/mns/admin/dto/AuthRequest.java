c438853d0b6e0a41763ee7cd25e29f078e6a51a0
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-28 20:57:32 +0100

    feat: creation de AuthRequest pour récuperer les paramètre des requêtes

---
+++ backend-admin-mns/src/main/java/com/mns/admin/dto/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.mns.admin.dto;
+
+public class AuthRequest {
+    private String email;
+    private String motDePasse;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+}
