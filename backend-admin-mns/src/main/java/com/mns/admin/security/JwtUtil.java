033d330e80777e00253c8bbf48250c00a58181c6
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-28 20:53:19 +0100

    feat: Creation de JwtUtil pour gérer la creation des tokens

---
+++ backend-admin-mns/src/main/java/com/mns/admin/security/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.mns.admin.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String SECRET;
+
+    private final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 heure
+
+    private Key getKey() {
+        if (SECRET.length() < 32) {
+            throw new IllegalArgumentException("Secret length must be at least 32 characters");
+        }
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token, String email) {
+        String tokenEmail = getEmailFromToken(token);
+        return email.equals(tokenEmail) && !isTokenExpired(token);
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(getKey()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parserBuilder().setSigningKey(getKey()).build()
+                .parseClaimsJws(token).getBody().getExpiration();
+        return expiration.before(new Date());
+    }
+}
