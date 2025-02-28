fcb41249d54a61397d72faa31800c08f618b26b0
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-28 14:38:12 +0100

    feat: ajout du script pour ouvrir la pop up compte

--- frontend-admin-mns/js/users.js
+++ frontend-admin-mns/js/users.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     window.users = users;
     initUsers();
     updateBreadcrumb();
+    initViewAccountModal();
 });
 
 function setActiveMenu() {
@@ -232,4 +233,25 @@ function initUsers() {
     filterRole.addEventListener("change", filterUsers);
 
     displayUsers();
+}
+
+function initViewAccountModal() {
+    const viewButton = document.querySelector(".button.view");
+    const modal = document.getElementById("viewAccountModal");
+    const closeModalButton = modal.querySelector(".close-btn");
+    // const form = document.getElementById("changeAccountForm");
+
+    viewButton.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+
+    closeModalButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
 }
