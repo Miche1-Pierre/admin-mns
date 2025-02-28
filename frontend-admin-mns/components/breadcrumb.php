05f6a4c26801e57c461a69a383bf250da297ef0f
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-02-28 14:37:40 +0100

    feat: ajout de la pop up pour le compte utilisateur

--- frontend-admin-mns/components/breadcrumb.php
+++ frontend-admin-mns/components/breadcrumb.php
@@ -12,7 +12,27 @@
             <small class="text-muted">Admin</small>
         </div>
         <div class="profile-photo">
-            <a href="/frontend-admin-mns/components/profile/profile.php"><img src="/frontend-admin-mns/assets/user1.png"></a>
+            <a class="button view"><img src="/frontend-admin-mns/assets/user1.png"></a>
+        </div>
+    </div>
+
+    <!-- Popup pour visualiser le compte -->
+    <div id="viewAccountModal" class="modal">
+        <div class="modal-content">
+            <span class="close-btn"><i class='bx bx-x'></i></span>
+            <h2>Compte</h2>
+            <form id="changeAccountForm">
+                <label for="photo">Photo de profil</label>
+                <input type="file" id="photo" name="photo">
+
+                <label for="utilisateur">Nom</label>
+                <input type="text" id="utilisateur" name="utilisateur">
+
+                <label for="email">Nom</label>
+                <input type="email" id="email" name="email">
+
+                <button type="submit" class="button">Appliquer les modifications</button>
+            </form>
         </div>
     </div>
 </div>
