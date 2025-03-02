ea44dbfc20130c62cca32fec9c7d50fda717d815
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-03-02 01:20:41 +0100

    refactor: ajustement des method deprecated

--- backend-admin-mns/src/main/java/com/mns/admin/config/SecurityConfig.java
+++ backend-admin-mns/src/main/java/com/mns/admin/config/SecurityConfig.java
@@ -20,14 +20,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated();
-
-        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/api/auth/login", "/api/auth/validate").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
