c6bb1fe1d12c711c63fd2207c33f251c0efaa2fc
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-03-03 13:48:18 +0100

    feat: ajout de variable d'env

--- backend-admin-mns/src/main/java/com/mns/admin/config/MailConfig.java
+++ backend-admin-mns/src/main/java/com/mns/admin/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.mns.admin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -7,19 +8,31 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailConfig {
+    @Value("${MAIL_HOST}")
+    private String host;
+
+    @Value("${MAIL_PORT}")
+    private int port;
+
+    @Value("${MAIL_USERNAME}")
+    private String username;
+
+    @Value("{MAIL_PASSWORD}")
+    private String password;
+
     @Bean
     public JavaMailSender getMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("sandbox.smtp.mailtrap.io");
-        mailSender.setPort(587);
-        mailSender.setUsername("fbcf9432dac195");
-        mailSender.setPassword("a6480ad4fe16b4");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         java.util.Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.starttls.required", "true");
-        props.put("mail.smtp.ssl.trust", "sandbox.smtp.mailtrap.io");
+        props.put("mail.smtp.ssl.trust", host);
 
         return mailSender;
     }
