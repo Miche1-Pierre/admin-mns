d4bf059a09e40d2881e3760a62c63328b89745a5
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-03-03 13:25:10 +0100

    feat: ajout de la route /verify-email

--- backend-admin-mns/src/main/java/com/mns/admin/controller/AuthController.java
+++ backend-admin-mns/src/main/java/com/mns/admin/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.mns.admin.dto.AuthRequest;
 import com.mns.admin.model.Utilisateur;
 import com.mns.admin.security.JwtUtil;
 import com.mns.admin.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,15 +25,23 @@ public class AuthController {
         return jwtUtil.validateToken(authRequest.getToken());
     }
 
+    @GetMapping("/verify-email")
+    public ResponseEntity<String> verifyEmail(@RequestParam String token) {
+        String jwt = userService.verifyEmail(token);
+        return ResponseEntity.ok("Email vérifié avec succès !");
+    }
+
     @PostMapping("/login")
-    public String login(@RequestBody AuthRequest authRequest) {
-        Utilisateur utilisateur = new Utilisateur();
-        utilisateur.setEmailUtilisateur(authRequest.getEmail());
-        utilisateur.setMotDePasseUtilisateur(authRequest.getMotDePasse());
-        Utilisateur authenticatedUser = userService.login(utilisateur);
-
-        String token = jwtUtil.generateToken(authenticatedUser.getEmailUtilisateur());
-
-        return token;
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+        try {
+            String token = userService.authenticateUser(authRequest);
+            return ResponseEntity.ok(token);
+        } catch (RuntimeException e) {
+            if (e.getMessage().equals("Veuillez vérifier votre email.")) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                        .body("Un email de vérification a été envoyé. Veuillez vérifier votre email.");
+            }
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
     }
 }
