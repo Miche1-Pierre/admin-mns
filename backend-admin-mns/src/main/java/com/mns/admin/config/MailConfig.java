ad55e35499d16fd8273dc4db3f0a49be2a54fb12
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-03-03 13:18:26 +0100

    feat: creation de la configuration de mailtrap

---
+++ backend-admin-mns/src/main/java/com/mns/admin/config/MailConfig.java
@@ -0,0 +1,26 @@
+package com.mns.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("sandbox.smtp.mailtrap.io");
+        mailSender.setPort(587);
+        mailSender.setUsername("fbcf9432dac195");
+        mailSender.setPassword("a6480ad4fe16b4");
+
+        java.util.Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.trust", "sandbox.smtp.mailtrap.io");
+
+        return mailSender;
+    }
+}
