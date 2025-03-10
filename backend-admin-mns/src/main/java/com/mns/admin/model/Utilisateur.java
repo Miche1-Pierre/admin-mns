65d7cffe0445f3e5bea7c8a07aece44159d6fcb0
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-03-10 13:47:43 +0100

    refactor: ajout d'un getter pour l'id utilisateur

--- backend-admin-mns/src/main/java/com/mns/admin/model/Utilisateur.java
+++ backend-admin-mns/src/main/java/com/mns/admin/model/Utilisateur.java
@@ -129,4 +129,8 @@ public class Utilisateur {
     public void setEmailVerified(boolean emailVerifie) {
         this.emailVerifie = emailVerifie;
     }
+
+    public Long getId() {
+        return idUtilisateur;
+    }
 }
