da74459a38a4b27d6ccea22175a334d754bcde00
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-03-16 14:06:13 +0100

    feat: creation des repository pour créer la route des candidatures

---
+++ backend-admin-mns/src/main/java/com/mns/admin/repository/FormationRepository.java
@@ -0,0 +1,10 @@
+package com.mns.admin.repository;
+
+import com.mns.admin.model.Formation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FormationRepository extends JpaRepository<Formation, Long> {
+    // continue
+}
