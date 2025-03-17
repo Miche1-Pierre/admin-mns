57385e5cabb6be1f15550a4adec54a87ec60b039
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-03-17 16:10:47 +0100

    feat: ajout d'une nouvelles methods

--- backend-admin-mns/src/main/java/com/mns/admin/config/WebConfig.java
+++ backend-admin-mns/src/main/java/com/mns/admin/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://admin-mns*", "http://admin-mns:8080*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
