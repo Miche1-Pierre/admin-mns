aeb52bcdf9d71deb657394a35c310adf7f1ab69c
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-05-29 23:39:10 +0200

    feat: add findByNomTypeDocument method to TypeDocumentRepository

--- backend-admin-mns/src/main/java/com/mns/admin/repository/TypeDocumentRepository.java
+++ backend-admin-mns/src/main/java/com/mns/admin/repository/TypeDocumentRepository.java
@@ -3,5 +3,8 @@ package com.mns.admin.repository;
 import com.mns.admin.model.TypeDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TypeDocumentRepository extends JpaRepository<TypeDocument, Long> {
+    Optional<TypeDocument> findByNomTypeDocument(String nomTypeDocument);
 }
