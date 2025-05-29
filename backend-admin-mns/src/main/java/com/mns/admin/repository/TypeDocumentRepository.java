afa235e9739a3e9269fdb156f29af3b78c989452
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-05-29 18:08:30 +0200

    feat: add TypeDocumentRepository interface for TypeDocument data access

---
+++ backend-admin-mns/src/main/java/com/mns/admin/repository/TypeDocumentRepository.java
@@ -0,0 +1,7 @@
+package com.mns.admin.repository;
+
+import com.mns.admin.model.TypeDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeDocumentRepository extends JpaRepository<TypeDocument, Long> {
+}
