71047c8f3b746ee3759f4c0b80ed8965458251be
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-05-29 23:39:06 +0200

    feat: add findByNomDocument and findByNomPhysique methods to DocumentRepository

--- backend-admin-mns/src/main/java/com/mns/admin/repository/DocumentRepository.java
+++ backend-admin-mns/src/main/java/com/mns/admin/repository/DocumentRepository.java
@@ -3,5 +3,10 @@ package com.mns.admin.repository;
 import com.mns.admin.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+    Optional<Document> findByNomDocument(String nomDocument);
+    Optional<Document> findByNomPhysique(String nomPhysique);
 }
